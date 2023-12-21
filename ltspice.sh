7f6b76201d3b40d1cd69d6cc3bf0d5828d08b684
Dennis Lee <dennis@dennislee.xyz>
2023-12-21 11:41:00 -0800

    Double quote to prevent globbing & word splitting

--- ltspice.sh
+++ ltspice.sh
@@ -4,9 +4,9 @@ CONFIG_DIR="${XDG_CONFIG_HOME:-$HOME/.config}/ltspice"
 if [ ! -d "$HOME"/.local/share/wineprefixes/ltspice ] ; then
    mkdir -p "$HOME"/.local/share/wineprefixes/ltspice
 fi
-if [ ! -d $CONFIG_DIR ]; then
-   mkdir -p $CONFIG_DIR
-   touch $CONFIG_DIR/LTspice.ini
+if [ ! -d "$CONFIG_DIR" ]; then
+   mkdir -p "$CONFIG_DIR"
+   touch "$CONFIG_DIR"/LTspice.ini
 fi
 export WINEPREFIX="${XDG_DATA_HOME:-$HOME/.local/share}"/wineprefixes/ltspice WINEARCH=win64
-wine /usr/lib/wine/x86_64-windows/start.exe /unix /opt/ltspice/LTspice.exe -ini $CONFIG_DIR/LTspice.ini "$@"
+wine /usr/lib/wine/x86_64-windows/start.exe /unix /opt/ltspice/LTspice.exe -ini "$CONFIG_DIR"/LTspice.ini "$@"
