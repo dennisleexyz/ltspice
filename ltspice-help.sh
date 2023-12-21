58b7eab6a5fbd66f86b79ebae7ecb9d76a0ad33c
Dennis Lee <dennis@dennislee.xyz>
2023-12-21 11:38:19 -0800

    Use ${XDG_DATA_HOME:-$HOME/.local/share}

--- ltspice-help.sh
+++ ltspice-help.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-export WINEPREFIX=$HOME/.local/share/wineprefixes/ltspice WINEARCH=win64
+export WINEPREFIX="${XDG_DATA_HOME:-$HOME/.local/share}"/wineprefixes/ltspice WINEARCH=win64
 wine hh /usr/share/doc/ltspice/ltspice.chm "$@"
--- ltspice.sh
+++ ltspice.sh
@@ -8,5 +8,5 @@ if [ ! -d $CONFIG_DIR ]; then
    mkdir -p $CONFIG_DIR
    touch $CONFIG_DIR/LTspice.ini
 fi
-export WINEPREFIX=$HOME/.local/share/wineprefixes/ltspice WINEARCH=win64
+export WINEPREFIX="${XDG_DATA_HOME:-$HOME/.local/share}"/wineprefixes/ltspice WINEARCH=win64
 wine /usr/lib/wine/x86_64-windows/start.exe /unix /opt/ltspice/LTspice.exe -ini $CONFIG_DIR/LTspice.ini "$@"
